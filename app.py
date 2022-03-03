b40673e32b110ae8ecd4fe53c33aedda710a542d
matharva <atharvamohite20@gmail.com>
2022-03-03 12:52:56 +0530

    updated with xray sdk

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-# Import the X-Ray modules
-# from aws_xray_sdk.ext.flask.middle
-# from time import sleep
-# from aws_xray_sdk.core import xray_recorder
-# from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+from time import sleep
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from flask import Flask
 # import requests
 
@@ -14,12 +12,12 @@ app = Flask(__name__)
 # app.config['DEBUG'] = True
 
 
-# # Configure the X-Ray recorder to generate segments with our service name
-# xray_recorder.configure(service='Local Server')
+# Configure the X-Ray recorder to generate segments with our service name
+xray_recorder.configure(service='Local Server')
 
-# # Instrument the Flask application
-# XRayMiddleware(app, xray_recorder)
-# xray_recorder.configure(context_missing='LOG_ERROR')
+# Instrument the Flask application
+XRayMiddleware(app, xray_recorder)
+xray_recorder.configure(context_missing='LOG_ERROR')
 
 
 @ app.route('/')
@@ -27,30 +25,30 @@ def hello_world():
     return {"data": "Server is running"}
 
 
-# @app.route("/callec2")
-# def call_ec2():
+@app.route("/test")
+def call_ec2():
 
-#     EC2_URL = "http://13.234.29.198:5000/"
+    # EC2_URL = "http://13.234.29.198:5000/"
 
-#     # Start a segment
-#     # segment = xray_recorder.begin_segment('ec2 segment')
+    # Start a segment
+    # segment = xray_recorder.begin_segment('ec2 segment')
 
-#     # Start a subsegment
-#     xray_recorder.begin_subsegment('Sleep subsegment')
+    # Start a subsegment
+    xray_recorder.begin_subsegment('Sleep subsegment')
 
-#     # Add metadata and annotations
-#     # segment.put_metadata('key', dict, 'namespace')
-#     # subsegment.put_annotation('key', 'value')
-#     sleep(1)
-#     xray_recorder.end_subsegment()
+    # Add metadata and annotations
+    # segment.put_metadata('key', dict, 'namespace')
+    # subsegment.put_annotation('key', 'value')
+    sleep(1)
+    xray_recorder.end_subsegment()
 
-#     xray_recorder.begin_subsegment('GET REQ subsegment')
-#     response = requests.get(EC2_URL)
-#     xray_recorder.end_subsegment()
+    # xray_recorder.begin_subsegment('GET REQ subsegment')
+    # # response = requests.get(EC2_URL)
+    # xray_recorder.end_subsegment()
 
-#     # Close the subsegment and segment
-#     # xray_recorder.end_segment()
-#     return {"local-data": response.json()}
+    # Close the subsegment and segment
+    # xray_recorder.end_segment()
+    return {"data": "Test end point"}
 
 
 if __name__ == "__main__":
